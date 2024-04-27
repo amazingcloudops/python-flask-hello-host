0c6f475cee3571d2bafd042fb30edec094fdb327
amazingclouops <amazingclouops@anonymous.com>
2024-04-27 15:54:06 +0530

    adding configmap

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
-import socket
+import socket, os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    host = socket.gethostname()
-    return render_template('index.html', host=host)
+    hostname = socket.gethostname()
+    custom_header = os.environ.get('CUSTOM_HEADER')
+    return render_template('index.html', hostname=hostname, custom_header=custom_header)
 
 if __name__ == '__main__':
     app.run()
